3b6f1f7931655055a503ae4a0174749203f9dc8e
YengSokroza <yengsokroza@gmail.com>
2024-04-20 23:16:22 +0700

    first commit

---
+++ app/(user)/test-jwt/page.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+'use client'
+
+export default function TestJwt() {
+    const [accessToken, setAccessToken] = useState("")
+    const handleLogin = async () => {
+        const email = "yengsokroza2023@gmail.com";
+        const password= "roza1234"
+    }
+  return (
+    <div>page</div>
+  )
+}
